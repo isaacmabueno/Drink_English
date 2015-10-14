d708c9b643d88250ea27dfb21d8ab0b50bfc17a3
Isaac Bueno <isaacmabueno@gmail.com>
2015-10-14 00:16:28 -0500

    social medias added

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
 $('.banner').fadeIn(4000);
 });
+$(document).ready(function () {
+  $(".navbar-nav li a").click(function(event) {
+    $(".navbar-collapse").collapse('hide');
+  });
+});
